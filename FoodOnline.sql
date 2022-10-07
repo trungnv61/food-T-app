2dad7c074a10d651fd241e3c8589f1383249c5d3
Trung-Van Nguyen <vantrung060101@gmail.com>
2022-10-07 20:53:33 +0700

    Update FoodOnline.sql
    arrange code

--- FoodOnline.sql
+++ FoodOnline.sql
@@ -2,24 +2,24 @@ create database FoodOnline
 
 create table Users (
     UserId int primary key identity(1, 1) not null,
-	Name varchar(50)  null,
-	UserName varchar(50) null unique,
-	Mobile varchar(50) null,
-	Email varchar(50) null unique,
-	Address varchar(max) null,
-	PostCode varchar(50) null,
-	Password varchar(50) null,
-	ImageUrl varchar(max) null,
-	CreatedDate datetime null
+    Name varchar(50)  null,
+    UserName varchar(50) null unique,
+    Mobile varchar(50) null,
+    Email varchar(50) null unique,
+    Address varchar(max) null,
+    PostCode varchar(50) null,
+    Password varchar(50) null,
+    ImageUrl varchar(max) null,
+    CreatedDate datetime null
 )
 
 create table Contact (
     ContactId int primary key identity(1, 1) not null,
-	Name varchar(50) null,
-	Email varchar(50) null,
-	Subject varchar(50) null,
-	Message varchar(max) null,
-	CreatedDate datetime null
+    Name varchar(50) null,
+    Email varchar(50) null,
+    Subject varchar(50) null,
+    Message varchar(max) null,
+    CreatedDate datetime null
 )
 
 create table Categories (
@@ -33,21 +33,21 @@ create table Categories (
 
 create table Products (
     ProductId int primary key identity(1, 1) not null,
-	Name varchar(50)  null,
-	Description varchar(max) null,
-	Price decimal(18, 2) null,
-	Quantity int null,
-	ImageUrl varchar(max) null,
-	CategoryId int null references Categories(CategoryId),
+    Name varchar(50)  null,
+    Description varchar(max) null,
+    Price decimal(18, 2) null,
+    Quantity int null,
+    ImageUrl varchar(max) null,
+    CategoryId int null references Categories(CategoryId),
     IsActive bit null,
     CreatedDate datetime null
 )
 
 create table Carts (
     CartId int primary key identity(1, 1) not null,
-	ProductId int null references Products(ProductId),
-	Quantity int null,
-	UserId int null references Users(UserId)
+    ProductId int null references Products(ProductId),
+    Quantity int null,
+    UserId int null references Users(UserId)
 )
 
 
@@ -64,11 +64,11 @@ create table Payment (
 
 create table Orders (
     OrderDetailsId int primary key identity(1, 1) not null,
-	OrderNo varchar(450) null unique,
-	ProductId int null references Products(ProductId),
+    OrderNo varchar(450) null unique,
+    ProductId int null references Products(ProductId),
     Quantity int null,
-	UserId int null references Users(UserId),
-	Status varchar(50) null,
-	PaymentId int null references Payment(PaymentId),
-	OrderDate datetime null
+    UserId int null references Users(UserId),
+    Status varchar(50) null,
+    PaymentId int null references Payment(PaymentId),
+    OrderDate datetime null
 )                   
