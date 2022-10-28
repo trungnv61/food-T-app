5b2d440ba78216fbcdc9db12449c09b5f7931309
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-10-28 22:24:02 +0700

    update Account Model

---
+++ Model/AccountModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Framework;
+
+namespace Model
+{
+   public class AccountModel
+    {
+        private FoodOnlineDbContext context = null;
+        public AccountModel()
+        {
+            context = new FoodOnlineDbContext();
+        }
+        public bool Login(string UserName, string Password)
+        {
+
+        }
+    }
+}
