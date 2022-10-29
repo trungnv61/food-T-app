0aba9b8941a9d68a1bae6894b4300cb7f7053a99
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-10-29 12:34:04 +0700

    update

--- WebFood/Areas/Admin/Models/LoginModel.cs
+++ WebFood/Areas/Admin/Models/LoginModel.cs
@@ -9,8 +9,8 @@ namespace WebFood.Areas.Admin.Models
     public class LoginModel
     {
         [Required]
-        public String UserName { get; set; }
-        public String Password { get; set; }
-        public String RememberMe { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
