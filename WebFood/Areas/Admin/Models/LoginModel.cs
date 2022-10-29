e363f5220b34aecfc021b32587e6b3fde3a6de83
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-10-29 17:11:45 +0700

    complete ui and login feature

--- WebFood/Areas/Admin/Models/LoginModel.cs
+++ WebFood/Areas/Admin/Models/LoginModel.cs
@@ -9,8 +9,8 @@ namespace WebFood.Areas.Admin.Models
     public class LoginModel
     {
         [Required]
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public bool RememberMe { get; set; }
+        public string UserName { set; get; }
+        public string Password { set; get; }
+        public bool RememberMe { set; get; }
     }
 }
