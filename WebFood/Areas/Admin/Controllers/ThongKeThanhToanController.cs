043cba08b02f6fd082f48ddc50e15635b262c62a
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-11-14 22:40:25 +0700

    update font and complete feature qltt

---
+++ WebFood/Areas/Admin/Controllers/ThongKeThanhToanController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Rotativa;
+using System.Web;
+using System.Web.Mvc;
+using Model.Dao;
+
+namespace WebFood.Areas.Admin.Controllers
+{
+    public class ThongKeThanhToanController : Controller
+    {
+        // GET: Admin/ThongKeThanhToan
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var dao = new PaymentDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        public ActionResult ExportPDF()
+        {
+            return new ActionAsPdf("Index")
+            {
+                FileName = Server.MapPath("~/Hinh/ThongKeThanhToan.pdf")
+            };
+
+        }
+
+        public void ExportExcel()
+        {
+
+        }
+    }
+}
